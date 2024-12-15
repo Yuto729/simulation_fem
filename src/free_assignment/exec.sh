8ff42f60ff7aea181faf2c5dbb7997fd424ffa6c
mitomi <mitomi.yuuto2003@gmail.com>
2024-12-16 01:39:04 +0900

    🔥 Refacutor for submit

--- src/free_assignment/exec.sh
+++ src/free_assignment/exec.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # コンパイル
-gcc -o ex6 ex6.c SolidFEM.c GLTool.c MathTool.c Mesh.c -lglut -lGLU -lGL -lm
+gcc -o main main.c SolidFEM.c GLTool.c MathTool.c Mesh.c -lglut -lGLU -lGL -lm
 
 # 実行
 if [ $? -eq 0 ]; then
-    ./ex6
+    ./main
 else
     echo "Compilation failed"
-fi 
+fi
--- src/test/exec.sh
+++
@@ -1,4 +0,0 @@
-# bin/bash
-set -e
-gcc test.c -o test -lglut -lGLU -lGL
-./test
